75e1f9bd26e950ab0a6826c06f663c071bb904c3
Jacob Swanson <jacob-swanson@users.noreply.github.com>
2023-07-04 08:37:23 -0500

    Test revert klipper change

--- pkgs/servers/klipper/klipper-firmware.nix
+++ pkgs/servers/klipper/klipper-firmware.nix
@@ -46,8 +46,7 @@
   installPhase = ''
     mkdir -p $out
     cp ./.config $out/config
-    cp out/klipper.bin $out/ || true
-    cp out/klipper.elf $out/ || true
+    cp -r out/* $out
   '';
 
   dontFixup = true;
